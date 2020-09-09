4e53ba0c6ddad6c1ffcae41de447d23be9cbbb40
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2020-09-09 17:17:30 +0100

    Improve benchmark

--- http/json/README.md
+++ http/json/README.md
@@ -9,15 +9,15 @@ The benchmark compares methods of generating a JSON response in Go with differen
 ## Results
 
 ```
-Benchmark/small_payload/direct-4  	   29007	     40616 ns/op	    1476 B/op	      14 allocs/op
-Benchmark/small_payload/bufio-4   	   29451	     40354 ns/op	    5647 B/op	      16 allocs/op
-Benchmark/small_payload/bufio_with_pooling-4         	   29834	     39225 ns/op	    1473 B/op	      14 allocs/op
-Benchmark/small_payload/buffer-4                     	   29067	     41536 ns/op	    1583 B/op	      16 allocs/op
-Benchmark/small_payload/buffer_with_pooling-4        	   28321	     47815 ns/op	    1472 B/op	      14 allocs/op
+Benchmark/small_payload/direct-4         	   28822	     40302 ns/op	    1477 B/op	      14 allocs/op
+Benchmark/small_payload/bufio-4          	   28213	     41653 ns/op	    5648 B/op	      16 allocs/op
+Benchmark/small_payload/bufio_pool-4     	   29620	     46394 ns/op	    1474 B/op	      14 allocs/op
+Benchmark/small_payload/buffer-4         	   23004	     62572 ns/op	    1578 B/op	      15 allocs/op
+Benchmark/small_payload/buffer_pool-4    	   29420	     39745 ns/op	    1473 B/op	      14 allocs/op
 
-Benchmark/large_payload/direct-4                     	    6988	    166522 ns/op	   73983 B/op	      19 allocs/op
-Benchmark/large_payload/bufio-4                      	    6586	    162004 ns/op	   78083 B/op	      21 allocs/op
-Benchmark/large_payload/bufio_with_pooling-4         	    6872	    158594 ns/op	   73880 B/op	      19 allocs/op
-Benchmark/large_payload/buffer-4                     	    5959	    181169 ns/op	  116799 B/op	      22 allocs/op
-Benchmark/large_payload/buffer_with_pooling-4        	    6915	    163763 ns/op	   76207 B/op	      19 allocs/op
+Benchmark/large_payload/direct-4         	    6552	    157061 ns/op	   74673 B/op	      19 allocs/op
+Benchmark/large_payload/bufio-4          	    6696	    161834 ns/op	   78287 B/op	      21 allocs/op
+Benchmark/large_payload/bufio_pool-4     	    7094	    156547 ns/op	   74394 B/op	      19 allocs/op
+Benchmark/large_payload/buffer-4         	    7082	    168776 ns/op	  116866 B/op	      22 allocs/op
+Benchmark/large_payload/buffer_pool-4    	    7168	    160605 ns/op	   76104 B/op	      19 allocs/op
 ```
--- http/json/bench_test.go
+++ http/json/bench_test.go
@@ -38,72 +38,95 @@ func init() {
 
 func Benchmark(b *testing.B) {
 	b.Run("small payload", func(b *testing.B) {
-		benchmark(b, &small)
+		benchmark(b, 27, &small)
 	})
 	b.Run("large payload", func(b *testing.B) {
-		benchmark(b, &large)
+		benchmark(b, 37545, &large)
 	})
 }
 
-func benchmark(b *testing.B, v interface{}) {
+func benchmark(b *testing.B, contentLen int, v interface{}) {
 	b.Run("direct", func(b *testing.B) {
-		benchServe(b, func(w http.ResponseWriter, r *http.Request) {
-			json.NewEncoder(w).Encode(v)
-		})
-	})
-
-	b.Run("bufio", func(b *testing.B) {
-		benchServe(b, func(w http.ResponseWriter, r *http.Request) {
-			bw := bufio.NewWriter(w)
-			json.NewEncoder(bw).Encode(v)
-		})
-	})
-
-	b.Run("bufio with pooling", func(b *testing.B) {
-		pool := sync.Pool{
-			New: func() interface{} { return bufio.NewWriter(nil) },
-		}
-
-		benchServe(b, func(w http.ResponseWriter, r *http.Request) {
-			bw := pool.Get().(*bufio.Writer)
-			bw.Reset(w)
-			defer pool.Put(bw)
-
-			json.NewEncoder(bw).Encode(v)
-		})
-	})
-
-	b.Run("buffer", func(b *testing.B) {
-		benchServe(b, func(w http.ResponseWriter, r *http.Request) {
-			bb := new(bytes.Buffer)
-			if err := json.NewEncoder(bb).Encode(v); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			} else {
-				bb.WriteTo(w)
+		benchServe(b, contentLen, func(w http.ResponseWriter, r *http.Request) {
+			if err := json.NewEncoder(w).Encode(v); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		})
 	})
 
-	b.Run("buffer with pooling", func(b *testing.B) {
+	b.Run("bufio", func(b *testing.B) {
+		benchServe(b, contentLen, func(w http.ResponseWriter, r *http.Request) {
+			bw := bufio.NewWriter(w)
+			if err := json.NewEncoder(bw).Encode(v); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if err := bw.Flush(); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		})
+	})
+
+	b.Run("bufio pool", func(b *testing.B) {
+		pool := sync.Pool{
+			New: func() interface{} { return bufio.NewWriter(nil) },
+		}
+
+		benchServe(b, contentLen, func(w http.ResponseWriter, r *http.Request) {
+			bw := pool.Get().(*bufio.Writer)
+			bw.Reset(w)
+			defer pool.Put(bw)
+
+			if err := json.NewEncoder(bw).Encode(v); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if err := bw.Flush(); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		})
+	})
+
+	b.Run("buffer", func(b *testing.B) {
+		benchServe(b, contentLen, func(w http.ResponseWriter, r *http.Request) {
+			bb := new(bytes.Buffer)
+			if err := json.NewEncoder(bb).Encode(v); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if _, err := bb.WriteTo(w); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		})
+	})
+
+	b.Run("buffer pool", func(b *testing.B) {
 		pool := sync.Pool{
 			New: func() interface{} { return new(bytes.Buffer) },
 		}
 
-		benchServe(b, func(w http.ResponseWriter, r *http.Request) {
+		benchServe(b, contentLen, func(w http.ResponseWriter, r *http.Request) {
 			bb := pool.Get().(*bytes.Buffer)
 			bb.Reset()
 			defer pool.Put(bb)
 
 			if err := json.NewEncoder(bb).Encode(v); err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-			} else {
-				bb.WriteTo(w)
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if _, err := bb.WriteTo(w); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
 		})
 	})
 }
 
-func benchServe(b *testing.B, fn http.HandlerFunc) {
+func benchServe(b *testing.B, contentLen int, fn http.HandlerFunc) {
 	srv := httptest.NewServer(http.HandlerFunc(fn))
 	defer srv.Close()
 
@@ -123,7 +146,10 @@ func benchServe(b *testing.B, fn http.HandlerFunc) {
 			b.Fatalf("expected no error, but received %v", err)
 		} else if code := res.StatusCode(); code != 200 {
 			b.Fatalf("expected 200 response but received %v", code)
+		} else if n := len(res.Body()); n != contentLen {
+			b.Fatalf("expected content length to be %d, but was %d", contentLen, n)
 		}
+
 		fasthttp.ReleaseRequest(req)
 		fasthttp.ReleaseResponse(res)
 	}
